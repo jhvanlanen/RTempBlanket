3b867983f3429a218e7d917d658ca415dfa11c7f
jhvanlanen <j.h.vanlanen@students.uu.nl>
2024-02-16 19:39:38 +0100

    -

--- RShinyTempBlanket.R
+++ RShinyTempBlanket.R
@@ -14,7 +14,7 @@ ui <- fluidPage(
                             /* changes the colour of the number tags */
                            .irs-from, .irs-to, .irs-single { background: #eeeeee }')),
   verticalLayout(
-  titlePanel("Make yourself a birth year temperature blanket"),
+  titlePanel("Make yourself a birth year - temperature blanket"),
   wellPanel(
     sliderInput("year", "What year were you born?", 1923, 2023,
                 value = 1995, step = 1, sep = "")
